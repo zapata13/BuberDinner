848d19c22551a9e49cf283d5d2b336da22b50f3c
Eduardo Rivas Zapata <zapata@redhat.com>
2022-09-02 17:58:36 -0600

    BuberDinner App Test

---
+++ BuberDinner.Contracts/Authentication/AuthenticationResponse.cs
@@ -0,0 +1,9 @@
+namespace BuberDinner.Contracts.Authentication;
+
+public record AuthenticationResponse(
+    Guid id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token
+);
